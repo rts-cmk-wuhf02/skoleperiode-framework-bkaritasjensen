64afe542bd6234be035ddc9bcf08f75911552956
Bianca Karitas jensen <5884546@rts-365.dk>
2020-04-23 08:35:06 +0200

    Update Build basic react files

--- Adopt_Me/src/App.js
+++ Adopt_Me/src/App.js
@@ -1,36 +1,26 @@
-	// your code is going to go here, for now
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
-	const Pet = ({ name, animal, breed }) => {
-		return React.createElement("div", {}, [
-			React.createElement("h1", {}, name),
-			React.createElement("h2", {}, animal),
-			React.createElement("h2", {}, breed)
-		]);
-	};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Ozzy",
+      animal: "Dog",
+      breed: "Pug",
+    }),
+    React.createElement(Pet, {
+      name: "Tino",
+      animal: "Dog",
+      breed: "Labrador",
+    }),
+    React.createElement(Pet, {
+      name: "Girlbert th. 2",
+      animal: "Cat",
+      breed: "Mix",
+    }),
+  ]);
+};
 
-	const App = () => {
-		return React.createElement(
-			"div",
-			{},
-			[
-				React.createElement("h1", {}, "Adopt Me!"),
-				React.createElement(Pet, {
-					name: "Ozzy", 
-					animal: "Dog", 
-					breed: "Pug"}),
-				React.createElement(Pet, {
-					name: "Tino", 
-					animal: "Dog", 
-					breed: "Labrador"}),
-				React.createElement(Pet, {
-					name: "Girlbert th. 2", 
-					animal: "Cat", 
-					breed: "Mix"})
-			]
-		);
-	};
-	
-	ReactDOM.render(
-		React.createElement(App),
-		document.getElementById("root")
-	);
+render(React.createElement(App), document.getElementById("root"));
