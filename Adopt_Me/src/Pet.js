19d323f4f18b0ec4881f992823e023018a61ab88
Bianca Karitas jensen <bianca_karitas_jensen@hotmail.com>
2020-04-23 19:52:01 +0200

    Update Package

--- Adopt_Me/src/Pet.js
+++ Adopt_Me/src/Pet.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// your code is going to go here, for now
+// your code is going to go here
 export default function Pet({ name, animal, breed }) {
   /*   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
