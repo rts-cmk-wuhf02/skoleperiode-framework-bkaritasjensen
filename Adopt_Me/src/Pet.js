6aaf76da8d69e2b58e44a97e6be049a875978156
Bianca Karitas jensen <bianca_karitas_jensen@hotmail.com>
2020-04-27 13:38:00 +0200

    Update Pet.js with Results

--- Adopt_Me/src/Pet.js
+++ Adopt_Me/src/Pet.js
@@ -1,18 +1,34 @@
 import React from "react";
 
 // your code is going to go here
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = `http://placecorgi.com/300/300`;
+  if (media.length) {
+    hero = media[0].small;
+  }
+
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
+  );
   /*   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed),
   ]); */
 
-  return (
+  /*  return (
     <div>
       <h1>{name}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
-  );
+  ); */
 }
